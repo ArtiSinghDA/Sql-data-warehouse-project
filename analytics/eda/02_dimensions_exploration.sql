ae5d705935d86f0538fc2714ae3dccc9388dffe5
Arti Singh <aartisingh96966@gmail.com>
2026-07-10 11:58:21 +0530

    Add EDA_dimensions_exploration.sql

--- analytics/eda/02_dimensions_exploration.sql
+++ analytics/eda/02_dimensions_exploration.sql
@@ -1,1 +1,23 @@
-
+/*
+===============================================================================
+STEP 2: DIMENSIONS EXPLORATION
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+    - Identify the distinct values (categories) for key business dimensions
+      such as customer country and product category.
+ 
+Tables Used:
+    - gold.dim_customers
+    - gold.dim_products
+===============================================================================
+*/
+ 
+-- Explore All Countries our customers come from
+SELECT DISTINCT country FROM gold.dim_customers;
+ 
+-- Explore All Categories "The major Divisions"
+SELECT DISTINCT category FROM gold.dim_products;
+ 
+SELECT DISTINCT category, subcategory, product_name FROM gold.dim_products
+ORDER BY 1, 2, 3;
